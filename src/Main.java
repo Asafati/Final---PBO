11e84d1535dca26dc8d7b0314217bb65027ed85e
Asafati Gea <2382006@unai.edu>
2024-11-26 21:51:52 +0700

    commit ke 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import repositories.TodoListRepository;
+import repositories.TodoListRepositoryImpl;
+import services.TodoListService;
+import services.TodoListServiceImpl;
+import views.TodoListTerminalViewImpl;
+import views.TodoListView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListTerminalViewImpl(todoListService);
+        todoListView.run();
+    } 
 }
